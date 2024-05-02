1b2a2e16f516ce5490fdb50e407f6adee9fec59b
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-05-02 14:10:29 +0530

    [Kaustubh Kale] Add: Function to check the last name entered is valid or not

--- User_Registration.py
+++ User_Registration.py
@@ -7,8 +7,20 @@ def valid_first_name(first_name):
     '''valid_first_name : function to check the name input is valid or not
         first_name : name entered by the user
          return : True if the regex pattern match
-         return : False if the pattern does not match'''
-    if re.match(r'^[A-Z].{2,}$', first_name):
+         return : False if the pattern does not match
+         '''
+    if re.match(r'^[A-Z][a-z]{2,}$', first_name):
+        return True
+    else:
+        return False
+
+def valid_last_name(last_name):
+    '''function to check the last_name 
+        last_name: last name entered by user
+        return : True if the pattern matches
+        return : False if the pattern does not match
+    '''
+    if re.match(r'[A-Z][a-z]{2,}$',last_name):
         return True
     else:
         return False
@@ -24,6 +36,13 @@ def main():
         else:
             logging.warning(f'Invalid first name: {first_name}')
             print("Invalid first name")
+        last_name=input("Enter last_name: ")
+        if valid_last_name(last_name):
+            logging.info(f"Valid Last name: {last_name}")
+            print("Valid last name")
+        else:
+            logging.warning(f"Invalid last name: {last_name}")
+            print("Invalid last name")
     except Exception as e:
         logging.log(e)
 
